6339f54f27789c067160ba09728b6b9a0f125288
JustVov4uk <volodabudzan4@gmail.com>
2026-04-20 12:02:39 +0200

    feat: add borrowings filtering and permissions

---
+++ borrowing/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+from borrowing.views import BorrowingViewSet
+
+app_name = "borrowing"
+
+router = DefaultRouter()
+router.register(r"borrowings", BorrowingViewSet)
+
+urlpatterns = router.urls
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingListSerializer, BorrowingCreateSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingListSerializer
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        else:
+            return BorrowingListSerializer
+
+    def get_queryset(self):
+        queryset = Borrowing.objects.all()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        is_active = self.request.query_params.get("is_active")
+        if is_active:
+            queryset = queryset.filter(actual_return_date__isnull=True)
+        user_id = self.request.query_params.get("user_id")
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+        return queryset
